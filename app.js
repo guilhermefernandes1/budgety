a1098b91c587abdbc149b3429d368ab5ae174b68
Guilherme Anjos <guilhermefernandes1@gmail.com>
2018-03-26 20:25:27 -0300

    Improving UX

--- app.js
+++ app.js
@@ -161,6 +161,12 @@ var UIController = (function() {
         dateLabel: '.budget__title--month'
     };
     
+    var nodeListForEach = function(list, callback){
+        for(var i=0; i<list.length; i++){
+            callback(list[i], i);
+        }
+    };
+    
     return {
         getInput: function(){
             return {
@@ -232,12 +238,6 @@ var UIController = (function() {
         displayPercentages: function(percentages) {
             var fields = document.querySelectorAll(DOMStrings.expensesPercentageLabel);
             
-            var nodeListForEach = function(list, callback){
-                for(var i=0; i<list.length; i++){
-                    callback(list[i], i);
-                }
-            };
-            
             nodeListForEach(fields, function(current, index){
                 if(percentages[index] > 0){
                     current.textContent = percentages[index] + '%';
@@ -278,6 +278,20 @@ var UIController = (function() {
             document.querySelector(DOMStrings.dateLabel).textContent = months[month] + '-' + year;
         },
         
+        changedType: function(){
+            var fields = document.querySelectorAll(
+                DOMStrings.inputType + ',' +
+                DOMStrings.inputDescription + ',' +
+                DOMStrings.inputValue
+            );
+            
+            nodeListForEach(fields, function(cur){
+                cur.classList.toggle('red-focus');
+            });
+            
+            document.querySelector(DOMStrings.inputBtn).classList.toggle('red');
+        },
+        
         getDOMStrings: function(){
             return DOMStrings;
         }
@@ -298,6 +312,8 @@ var controller = (function(budgetCtrl, UICtrl) {
         });
         
         document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+        
+        document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
     };
     
     var DOM = UICtrl.getDOMStrings();
